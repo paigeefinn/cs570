9b6862a4f9bf2a13f4a8c55bcf0419e4b91628fc
pfinn <paigedoherty@Paiges-MacBook-Pro-2.local>
2020-06-13 18:46:50 -0700

    feature: file creation in main

--- src.c
+++ src.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
+#define NUM_THREADS     7
 
-void *entry_point(void *value)
+void *odd_threads(void *value)
 {
   printf("second thrAD\n");
   
   int *num = (int *) value;
   
-  printf("the valuue of value is %d", *num);
+  printf("the value of value is %d", *num);
   
   return NULL;
  }
 
+
+
 int main(int argc, char **argv)
 {
+  # assigns a file pointer and creates the txt file QUOTE
+  FILE *fp;
+  fp = fopen ("QUOTE.txt", "w")
+  
   pthread_t thread;
   
   printf("first thread \n ");
   
   int num = 5;
   
-  pthread_create(&thread, NULL, entry_point, &num);
+  pthread_create(&thread, NULL, odd_threads, &num);
   
   pthread_join(thread, NULL);
   
